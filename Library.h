cae89b3c20018139215d2172f6d973e6b2ead9c4
Alex Gable <agable@ithaca.edu>
2017-11-30 23:18:49 -0500

    Added a few functions to Library header

--- Library.h
+++ Library.h
@@ -7,6 +7,8 @@
 
 #include "People.h"
 #include "Book.h"
+#include <iostream>
+#include <fstream>
 
 class Library{
 private:
@@ -14,8 +16,14 @@ private:
     List<People> memberList;
     //pointer to list of books in library
     List<Book> inBooks;
+    //fstream parts
+    std::ifstream inBooksIN;
+    std::ofstream inBooksOUT;
     //pointer to list of books checked out
     List<Book> outBooks;
+    //fstream parts
+    std::ifstream outBooksIN;
+    std::ofstream outBooksOUT;
 
 public:
     /**
@@ -41,5 +49,29 @@ public:
      * Destructor
      */
     ~ArrayList();
+
+    /**
+     * Creates a new library member (Person) given prompts about information
+     * Adds to the memberList
+     * @return true if successful, false if unsuccessful
+     */
+    bool addMember();
+
+    /**
+     * Save the inventory and wait lists in a file and terminate execution.
+     */
+    void quit();
+
+    /**
+     * Adds book to inBooks list
+     * @param bookToAdd
+     */
+    void addBook(Book bookToAdd);
+
+    /**
+     * Moves a book from the outBooks list to the inBooks list
+     * @param bookToReturn
+     */
+    void returnBook(Book bookToReturn);
 };
 #endif //FINALPROJECT_LIBRARY_H
