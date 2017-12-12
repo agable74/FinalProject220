9fa70e3515351091ae0f774fcd9887cbe296a4f1
Ben Hamula <bhamula1@ithaca.edu>
2017-12-12 12:46:12 -0500

    Minor updates Member.h and .cpp

---
+++ Member.h
@@ -0,0 +1,52 @@
+//
+// Created by Ben on 12/4/2017.
+//
+
+#ifndef FINALPROJECT_MEMBER_H
+#define FINALPROJECT_MEMBER_H
+
+#include <string>
+
+class Member {
+
+private:
+
+    std::string name;
+    std::string id;
+    long phoneNumber;
+    std::string email;
+    std::string contactPref;
+    std::string generateId();
+
+public:
+
+    /**
+     * Default Constructor
+     */
+    Member();
+    /**
+     * Constructor
+     */
+    Member(std::string nameIn, long long phoneNumberIn, std::string emailIn, std::string idIn, std::string contactPrefIn);
+    /**
+     * Copy Constructor
+     * @param memberToCopy
+     */
+    Member(const Member& memberToCopy);
+    /**
+     * Assignment Operator
+     */
+    Member&operator=(const Member& memberToCopy);
+
+    std::string getName();
+    std::string getId();
+    long long getPhoneNumber();
+    std::string getEmail();
+    std::string setContactPref();
+    std::string getContactPref();
+    std::string toString();
+
+
+};
+
+#endif //FINALPROJECT_MEMBER_H
